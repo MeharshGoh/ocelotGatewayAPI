796b2902e96049079203fbcd828b4339f7e840a3
Amol Sawkar <Amol.Sawkar@citiustech.com>
2021-07-08 15:46:20 +0530

    Add project files.

---
+++ ProductService/Controllers/ProductAPIController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+
+        ProductsDb products;
+        public ProductAPIController()
+        {
+            products = new ProductsDb();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product prd)
+        {
+            products.Add(prd);
+            return Ok(products);
+        }
+
+
+    }
+}
---
+++ VendorService/Controllers/VendorAPIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendorService.Models;
+
+namespace VendorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorAPIController : ControllerBase
+    {
+        VendorsDb vendors;
+
+        public VendorAPIController()
+        {
+            vendors = new VendorsDb();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(vendors);
+        }
+    }
+}
